ad949ae6cf7bd79d0538e0b83ad4b72a4dd38205
vidyasagarreddy40 <vidyasagarreddy40@gmail.com>
2024-04-09 20:20:45 +0530

    String reverse recursion

---
+++ JavaSDETPrograms/src/main/java/programs/stringRevRecurssion.java
@@ -0,0 +1,27 @@
+package programs;
+
+public class stringRevRecurssion {
+
+    static String word = "radar", rev = "", temp = word;
+    static int len=word.length();
+
+    public static void main(String[] args) {
+
+        revRecursion();
+        if(temp.equalsIgnoreCase(rev)){
+            System.out.println(temp+" is a palindrome");
+        }
+        else{
+            System.out.println(temp+" is not a palindrome");
+        }
+    }
+
+    static void revRecursion(){
+        len--;
+        if(len>=0){
+            rev=rev+word.charAt(len);
+            revRecursion();
+        }
+    }
+
+}
