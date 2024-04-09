99a8cebaf029fcde43dfcf0b28f7876933779946
vidyasagarreddy40 <vidyasagarreddy40@gmail.com>
2024-04-09 17:32:56 +0530

    prime numbers

---
+++ JavaSDETPrograms/src/main/java/programs/PrimeNumbers.java
@@ -0,0 +1,20 @@
+package programs;
+
+public class PrimeNumbers {
+
+    public static void main(String[]args){
+        int num=100, counter;
+
+        for(int i=2;i<=num;i++){
+            counter=0;
+            for(int j=1;j<=num;j++){
+                if(i%j==0){
+                    counter++;
+                }
+            }
+            if(counter==2){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+}
